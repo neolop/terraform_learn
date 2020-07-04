47101291c354e629d3592f461392b3cd5ef32f10
Jorge Lopez <jorge.lopez@cencosud.cl>
2020-07-03 23:11:20 -0400

    Deploy a Configurable Single Web Server

--- single_server/main.tf
+++ single_server/main.tf
@@ -9,22 +9,19 @@ provider "aws" {
   version = "~> 2.0"
 }
 
-
-
-resource "aws_launch_configuration" "example" {
-  image_id               = "ami-0c55b159cbfafe1f0"
+resource "aws_instance" "example" {
+  ami                    = "ami-0c55b159cbfafe1f0"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
+
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
-  # Required when using a launch configuration with an auto scaling group.
-  # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
-  lifecycle {
-    create_before_destroy = true
+  tags = {
+    Name = "terraform-example"
   }
 }
 
@@ -45,15 +42,8 @@ variable "server_port" {
   default     = 8080
 }
 
-resource "aws_autoscaling_group" "example" {
-  launch_configuration = aws_launch_configuration.example.name
 
-  min_size = 2
-  max_size = 10
-
-  tag {
-    key                 = "Name"
-    value               = "terraform-asg-example"
-    propagate_at_launch = true
-  }
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP address of the Web Server"
 }
